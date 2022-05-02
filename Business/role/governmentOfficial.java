8e39627c0334c34070e064f7ab1a2128c5f0667a
Rucha Mangalekar <98709398+rucham42@users.noreply.github.com>
2022-05-01 23:34:32 -0400

    Update governmentOfficial.java

--- Business/role/governmentOfficial.java
+++ Business/role/governmentOfficial.java
@@ -12,7 +12,7 @@ import userinterface.CustomerRole.CustomerAreaJPanel;
 
 /**
  *
- * @author ayushi
+ * @author Rucha Mangalekar
  */
 public class governmentOfficial extends Role{
     public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, EcoSystem business) {
